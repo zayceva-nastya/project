858d6473bf68c6018cf333fa99430a4fefccd880
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-08-25 19:27:38 +0300

    db

--- install/_fitness.sql
+++ install/_fitness.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Авг 18 2020 г., 18:45
+-- Время создания: Авг 25 2020 г., 19:03
 -- Версия сервера: 10.3.22-MariaDB
 -- Версия PHP: 7.4.5
 
@@ -22,6 +22,26 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `project` DEFAULT CHARACTER SET utf16 COLLATE utf16_bin;
 USE `project`;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `diary`
+--
+
+CREATE TABLE `diary` (
+  `users_id` int(11) NOT NULL COMMENT '№',
+  `exercise` varchar(500) COLLATE utf16_bin NOT NULL COMMENT 'Упражнение',
+  `lead time` varchar(50) COLLATE utf16_bin NOT NULL COMMENT 'время выполнения',
+  `date` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'Дата'
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Дамп данных таблицы `diary`
+--
+
+INSERT INTO `diary` (`users_id`, `exercise`, `lead time`, `date`) VALUES
+(30, 'прыжки', '25мин', '2020-08-21 19:40:16');
 
 -- --------------------------------------------------------
 
@@ -46,20 +66,6 @@ INSERT INTO `group` (`id`, `name`, `cod`) VALUES
 -- --------------------------------------------------------
 
 --
--- Структура таблицы ` trainingdiary`
---
-
-CREATE TABLE ` trainingdiary` (
-  `user_id` int(11) NOT NULL COMMENT '№',
-  `exercise` varchar(500) COLLATE utf16_bin NOT NULL COMMENT 'Упражнение',
-  `phonenumber` varchar(50) COLLATE utf16_bin NOT NULL COMMENT 'Телефон',
-  `email` varchar(50) COLLATE utf16_bin NOT NULL COMMENT 'Почта',
-  `name` varchar(50) COLLATE utf16_bin NOT NULL COMMENT 'Имя'
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
-
--- --------------------------------------------------------
-
---
 -- Структура таблицы `users`
 --
 
@@ -71,8 +77,20 @@ CREATE TABLE `users` (
   `FIO` varchar(150) NOT NULL COMMENT 'ФИО',
   `Weight` int(11) NOT NULL COMMENT 'Вес',
   `Growth` int(11) NOT NULL COMMENT 'Рост',
-  `Gender` int(11) NOT NULL COMMENT 'Пол'
+  `Gender` varchar(11) NOT NULL COMMENT 'Пол'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `password`, `group_id`, `FIO`, `Weight`, `Growth`, `Gender`) VALUES
+(30, 'start', '123', 1, 'AAA BBB CCC', 68, 167, '1'),
+(31, 'finish', '987', 1, 'DD DD  DD', 89, 158, '1'),
+(32, 'tyt', 'rty', 1, ' rtyr ty r ty', 89, 188, 'women'),
+(33, 'yrtyrt', 'tyrt rty ', 2, 'yt rty rty', 89, 158, 'men'),
+(34, 'Pavel', '987', 2, 'DD DD  DD', 78, 225, 'М'),
+(35, 'Vasya', '569', 2, 'ерке ке ке ', 99, 185, 'М');
 
 -- --------------------------------------------------------
 
@@ -88,8 +106,21 @@ CREATE TABLE `workouts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
 
 --
+-- Дамп данных таблицы `workouts`
+--
+
+INSERT INTO `workouts` (`user_id`, `exercise`, ` number of repetitions`, ` lead time`) VALUES
+(30, 'Бег', '-', '10мин');
+
+--
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `diary`
+--
+ALTER TABLE `diary`
+  ADD PRIMARY KEY (`users_id`);
 
 --
 -- Индексы таблицы `group`
@@ -97,12 +128,6 @@ CREATE TABLE `workouts` (
 ALTER TABLE `group`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id` (`id`);
-
---
--- Индексы таблицы ` trainingdiary`
---
-ALTER TABLE ` trainingdiary`
-  ADD PRIMARY KEY (`user_id`);
 
 --
 -- Индексы таблицы `users`
@@ -129,32 +154,26 @@ ALTER TABLE `group`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '№', AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT для таблицы ` trainingdiary`
---
-ALTER TABLE ` trainingdiary`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '№', AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '№', AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '№', AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT для таблицы `workouts`
 --
 ALTER TABLE `workouts`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '№', AUTO_INCREMENT=10;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '№', AUTO_INCREMENT=31;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
 
 --
--- Ограничения внешнего ключа таблицы ` trainingdiary`
+-- Ограничения внешнего ключа таблицы `diary`
 --
-ALTER TABLE ` trainingdiary`
-  ADD CONSTRAINT ` trainingdiary_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+ALTER TABLE `diary`
+  ADD CONSTRAINT `diary_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`);
 
 --
 -- Ограничения внешнего ключа таблицы `users`
