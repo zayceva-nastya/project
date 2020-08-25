ada978d897c076117995977dccc8336a42c366cb
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-08-25 21:03:42 +0300

    js

---
+++ public/JS/script.js
@@ -0,0 +1,6 @@
+document.getElementById("addButton").onclick = function () {
+    document.getElementById("addForm").classList.toggle("hidden")
+    this.innerText="clear"
+}
+
+document.getElementById("addForm").onclick
